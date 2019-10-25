764e9ff225c2b6e93aa4f617b1210b0a14b90b04
Spencer Markin <smarkin@questica.com>
2019-10-25 16:56:11 -0400

    move tray logic to timer_tray subclass

--- app/timer_tray.js
+++ app/timer_tray.js
@@ -1,8 +1,30 @@
 const { Tray } = require('electron');
 
 class TimerTray extends Tray {
-  constructor(iconPath) {
+  constructor(iconPath, mainWindow) {
     super(iconPath);
+    this.mainWindow = mainWindow;
+    this.on('click', this.onClick.bind(this));
+  }
+
+  onClick(event, bounds) {
+    const { x, y } = bounds;
+    const { height, width } = this.mainWindow.getBounds();
+
+    if (this.mainWindow.isVisible()) {
+      this.mainWindow.hide();
+    } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+
+      this.mainWindow.setBounds({
+        height,
+        width,
+        y: yPosition,
+        x: x - width / 2
+      });
+
+      this.mainWindow.show();
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -22,25 +22,5 @@ app.on('ready', () => {
     process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  trayIcon = new TimerTray(iconPath);
-
-  trayIcon.on('click', (event, bounds) => {
-    const { x, y } = bounds;
-    const { height, width } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      const yPosition = process.platform === 'darwin' ? y : y - height;
-
-      mainWindow.setBounds({
-        height,
-        width,
-        y: yPosition,
-        x: x - width / 2
-      });
-
-      mainWindow.show();
-    }
-  });
+  trayIcon = new TimerTray(iconPath, mainWindow);
 });
